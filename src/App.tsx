9615c7396ea08bc081dd5e8940c431d5a27840da
Sayan1952 <sprontu@gmail.com>
2024-04-17 23:52:29 +0600

    initial-routes-demo

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
 import { authProvider, dataProvider, liveProvider } from "./providers";
-import { Home } from "./pages";
+import { Home, ForgotPassword, Login, Register } from "./pages";
 
 
 import routerBindings, {
